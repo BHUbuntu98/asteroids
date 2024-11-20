b4a5e74e3749a77ed3be2ef54f88ba793762a847
BHubuGit <ben.hand@rocketmail.com>
2024-11-20 13:31:02 -0500

    Finished basic asteroids

--- main.py
+++ main.py
@@ -2,22 +2,68 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-
+from constants import *
+from player import Player
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from asteroid import *
+from asteroidfield import *
+import sys
+from firing_my_laser import Shot
+
+
+
+
+
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    Shot.containers = (shots, updatable, drawable)
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        pygame.Surface.fill(screen, (0, 0, 0))
-        pygame.display.flip() 
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        for obj in updatable:
+            obj.update(dt)
+
+        for asteroid_obj in asteroids:
+            for bullet in shots:
+                if bullet.collide(asteroid_obj):
+                    bullet.kill()
+                    asteroid_obj.split()
+            if asteroid_obj.collide(player):
+                print("Game over!")
+                sys.exit()
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 
 if __name__ == "__main__":
